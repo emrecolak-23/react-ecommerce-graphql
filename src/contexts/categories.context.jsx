3a5dd04777d44d5718004bb764024fa06ca71939
Emre Çolak <emrecolak@Emre.home>
2023-03-07 20:13:06 +0300

    updating components to consume graphql values

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -25,10 +25,22 @@ export const CategoriesProvider = ({ children }) => {
   const {loading, error, data} = useQuery(COLLECTIONS)
   const [categoriesMap, setCategoriesMap] = useState({});
 
-  console.log('loading', loading)
-  console.log('data', data)
+  useEffect(() => {
+    if(data) {
+      const {collections} = data
+      const collectionMap = collections.reduce((acc, collection) => {
+        const {title, items} = collection
+        acc[title.toLowerCase()] = items
 
-  const value = { categoriesMap };
+        return acc
+      }, {})
+      
+      setCategoriesMap(collectionMap)
+    }
+  }, [data])
+
+
+  const value = { categoriesMap, loading };
   return (
     <CategoriesContext.Provider value={value}>
       {children}
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -6,10 +6,11 @@ import ProductCard from '../../components/product-card/product-card.component';
 import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
+import Spinner from '../../components/spinner/spinner.component';
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const { categoriesMap, loading } = useContext(CategoriesContext);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -19,12 +20,14 @@ const Category = () => {
   return (
     <Fragment>
       <Title>{category.toUpperCase()}</Title>
-      <CategoryContainer>
+      { loading ? <Spinner/> : 
+        <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </CategoryContainer>
+        </CategoryContainer>
+      }
     </Fragment>
   );
 };
